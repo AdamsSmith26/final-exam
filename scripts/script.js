24504c59dfb2183e15fa7b5bf050a4c870169e80
Adam <adamssmith2003@gmail.com>
2023-05-23 20:06:16 +0400

    section-4

--- scripts/script.js
+++ scripts/script.js
@@ -1,107 +1,156 @@
 // burger menu
-document.getElementById("burger").addEventListener("click", function(){
-    document.getElementsByClassName("mynav")[0].classList.toggle("open");
-    document.getElementsByClassName("nav-btn")[0].classList.toggle("myopacity");
+document.getElementById("burger").addEventListener("click", function () {
+  document.getElementsByClassName("mynav")[0].classList.toggle("open");
+  document.getElementsByClassName("nav-btn")[0].classList.toggle("myopacity");
 });
 
-window.addEventListener('DOMContentLoaded', () => { 
+window.addEventListener('DOMContentLoaded', () => {
 
-    const count = new CountUp( 
-      'num1', 
-      0, 
-      8.9, 
-      1, 
-      3 
-    );
-    count.start() 
+  const count = new CountUp(
+    'num1',
+    0,
+    8.9,
+    1,
+    3
+  );
+  count.start()
 
-    const count1 = new CountUp( 
-      'num2', 
-      0, 
-      65, 
-      0, 
-      3 
-    );
-    count1.start() 
+  const count1 = new CountUp(
+    'num2',
+    0,
+    65,
+    0,
+    3
+  );
+  count1.start()
 
-    const count2 = new CountUp( 
-      'num3', 
-      0, 
-      87, 
-      0, 
-      3 
-    );
-    count2.start() 
-  })
+  const count2 = new CountUp(
+    'num3',
+    0,
+    87,
+    0,
+    3
+  );
+  count2.start()
+})
 
-  function startTimer(duration, display) {
-    var timer = duration, hourses, minutes, seconds;
-    setInterval(function () {
-        hourses = parseInt(timer / 3600), 10
-        minutes = parseInt(timer % 3600 / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+function startTimer(duration, display) {
+  var timer = duration, hourses, minutes, seconds;
+  setInterval(function () {
+    hourses = parseInt(timer / 3600), 10
+    minutes = parseInt(timer % 3600 / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-        hourses = hourses < 10 ? "0" + hourses : hourses;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    hourses = hourses < 10 ? "0" + hourses : hourses;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = hourses + ":" + minutes + ":" + seconds;
+    display.textContent = hourses + ":" + minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
 }
 
 window.onload = function () {
-    var myTime = 5*3600 + 60 * 45 + 47,
-        display = document.querySelector('#time');
-    startTimer(myTime, display);
+  var myTime = 5 * 3600 + 60 * 45 + 47,
+    display = document.querySelector('#time');
+  startTimer(myTime, display);
 };
 
 var card = document.getElementById('card-left');
-    window.addEventListener('scroll', async function () {
-        if (window.scrollY  > 200) {
-            card.classList.add("animate__zoomInLeft");
-        } else {
-            card.classList.remove("animate__zoomInLeft");
-        }
-    });
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 200) {
+    card.classList.add("animate__zoomInLeft");
+  } else {
+    card.classList.remove("animate__zoomInLeft");
+  }
+});
 
 var card1 = document.getElementById('card-center');
-    window.addEventListener('scroll', async function () {
-        if (window.scrollY  > 200) {
-            card1.classList.add("animate__zoomIn");
-        } else {
-            card1.classList.remove("animate__zoomIn");
-        }
-    });
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 200) {
+    card1.classList.add("animate__zoomIn");
+  } else {
+    card1.classList.remove("animate__zoomIn");
+  }
+});
 
 var card2 = document.getElementById('card-right');
-    window.addEventListener('scroll', async function () {
-        if (window.scrollY  > 200) {
-            card2.classList.add("animate__zoomInRight");
-        } else {
-            card2.classList.remove("animate__zoomInRight");
-        }
-    });
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 200) {
+    card2.classList.add("animate__zoomInRight");
+  } else {
+    card2.classList.remove("animate__zoomInRight");
+  }
+});
 
-  
-    var photo1 = document.getElementById('photo1');
-    window.addEventListener('scroll', async function () {
-        if (window.scrollY  >  1000) {
-            photo1.classList.add("animate__fadeInUp");
-        } else {
-            photo1.classList.remove("animate__fadeInUp");
-        }
-    });
 
-    var photo2 = document.getElementById('photo2');
-    window.addEventListener('scroll', async function () {
-        if (window.scrollY  >  1000) {
-            photo2.classList.add("animate__fadeInDown");
-        } else {
-            photo2.classList.remove("animate__fadeInDown");
-        }
-    });
+var photo1 = document.getElementById('photo1');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1000) {
+    photo1.classList.add("animate__fadeInUp");
+  } else {
+    photo1.classList.remove("animate__fadeInUp");
+  }
+});
+
+var photo2 = document.getElementById('photo2');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1000) {
+    photo2.classList.add("animate__fadeInDown");
+  } else {
+    photo2.classList.remove("animate__fadeInDown");
+  }
+});
+
+var img1 = document.getElementById('img1');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1700) {
+    img1.classList.add("animate__fadeIn");
+  } else {
+    img1.classList.remove("animate__fadeIn");
+  }
+});
+
+var img2 = document.getElementById('img2');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1700) {
+    img2.classList.add("animate__fadeIn");
+  } else {
+    img2.classList.remove("animate__fadeIn");
+  }
+});
+
+var img3 = document.getElementById('img3');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1700) {
+    img3.classList.add("animate__fadeIn");
+  } else {
+    img3.classList.remove("animate__fadeIn");
+  }
+});
+
+var img4 = document.getElementById('img4');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1700) {
+    img4.classList.add("animate__fadeIn");
+  } else {
+    img4.classList.remove("animate__fadeIn");
+  }
+});
+
+var img5 = document.getElementById('img5');
+window.addEventListener('scroll', async function () {
+  if (window.scrollY > 1700) {
+    img5.classList.add("animate__fadeIn");
+  } else {
+    img5.classList.remove("animate__fadeIn");
+  }
+});
+
+
+
+
 
